ad9c95512d63afee1e4a395a3e33ca78696e6352
Mehul Goel <mehulgoel873@gmail.com>
2024-07-12 00:28:14 -0700

    finished prior app demo

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
-
+  var favorites = <WordPair>[];
   File? _selectedImage;
 
   void getNext() {
@@ -75,58 +75,123 @@ class MyAppState extends ChangeNotifier {
       print(response.text);
     }
   }
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
-class MyHomePage extends StatelessWidget {
+// ...
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for selected index');
+    }
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: false,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
-    return Scaffold(
-      body: Column(
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('A random idea: And a random answer'),
           BigCard(pair: pair),
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
           ),
-          MaterialButton(
-              color: Colors.red,
-              child: const Text("Pick Image from Camera",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16)),
-              onPressed: () {
-                print("Capture Image Button Pressed");
-                appState.pickImageFromCamera();
-              }),
-          MaterialButton(
-              color: Colors.red,
-              child: const Text("Initialize Gen AI Model",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16)),
-              onPressed: () {
-                print("Pressed GEN AI Button");
-                appState.startGenAI();
-              }),
-          const SizedBox(
-            height: 20,
-          ),
-          appState._selectedImage != null
-              ? Image.file(appState._selectedImage!)
-              : Text("Please select an image ")
         ],
       ),
     );
   }
 }
+
+// ...
 
 class BigCard extends StatelessWidget {
   const BigCard({
@@ -155,3 +220,32 @@ class BigCard extends StatelessWidget {
     );
   }
 }
+
+// MaterialButton(
+//     color: Colors.red,
+//     child: const Text("Pick Image from Camera",
+//         style: TextStyle(
+//             color: Colors.white,
+//             fontWeight: FontWeight.bold,
+//             fontSize: 16)),
+//     onPressed: () {
+//       print("Capture Image Button Pressed");
+//       appState.pickImageFromCamera();
+//     }),
+// MaterialButton(
+//     color: Colors.red,
+//     child: const Text("Initialize Gen AI Model",
+//         style: TextStyle(
+//             color: Colors.white,
+//             fontWeight: FontWeight.bold,
+//             fontSize: 16)),
+//     onPressed: () {
+//       print("Pressed GEN AI Button");
+//       appState.startGenAI();
+//     }),
+// const SizedBox(
+//   height: 20,
+// ),
+// appState._selectedImage != null
+//     ? Image.file(appState._selectedImage!)
+//     : Text("Please select an image ")
